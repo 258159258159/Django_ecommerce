e12dc93305594651b26175e84ae0ff6cd7d0c5d3
Shelton Oliveira <shelton.oliveira@gmail.com>
2023-04-24 09:10:27 -0300

    create context variable title and content to all

--- e_commerce/e_commerce/views.py
+++ e_commerce/e_commerce/views.py
@@ -2,21 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def home_page(request):
     context = {
-        "title": "Página principal",
-        "content": "Bem-vindo a página principal"
+        "title": "Página Principal",
+        "content": "Bem-vindo a Página Principal"
     }
     return render(request, "home_page.html", context)
 
 def about_page(request):
     context = {
-        "title": "Página sobre",
-        "content": "Bem-vindo a página sobre"
+        "title": "Página Sobre",
+        "content": "Bem-vindo a Página Sobre"
     }
     return render(request, "about/view.html", context)
 
 def contact_page(request):
     context = {
-        "title": "Página de contato",
-        "content": "Bem-vindo a página de contato"
+        "title": "Página de Contato",
+        "content": "Bem-vindo a Página de Contato"
     }
     return render(request, "contact/view.html", context)
