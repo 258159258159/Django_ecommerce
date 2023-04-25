6408188b2053004a80310ccddcb2ca582efdecd0
Shelton Oliveira <shelton.oliveira@gmail.com>
2023-04-25 16:07:58 -0300

    atualizations in paste

--- products/models.py
+++ products/models.py
@@ -18,7 +18,7 @@ class ProductManager(models.Manager):
         return self.get_queryset().active()
 
     def featured(self):
-        #return self.get_queryset().filter(featured = True)
+        #self.get_queryset().filter(featured = True)
         return self.get_queryset().featured()
 
     def get_by_id(self, id):
